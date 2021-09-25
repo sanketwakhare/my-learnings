5382e65430fa58dddbf795b3bd3018d8b28885b7
Sanket <sanketwakhare@gmail.com>
2021-09-25 13:09:06 +0530

    rotating game q4 java

---
+++ scaler_problems/day10-array-dynamic-array-intro/Q4_RotatingGame.java
@@ -0,0 +1,34 @@
+package com.abc;
+
+import java.util.Scanner;
+
+public class Q4_RotatingGame {
+
+	public static void main(String[] args) {
+		// YOUR CODE GOES HERE
+		// Please take input and print output to standard input/output (stdin/stdout)
+		// DO NOT USE ARGUMENTS FOR INPUTS
+		// E.g. 'Scanner' for input & 'System.out' for output
+		Scanner scanner = new Scanner(System.in);
+		String line1 = scanner.nextLine();
+		String line2 = scanner.nextLine();
+		// store elements in array
+		String[] tokens = line1.split(" ");
+		int arrayLength = Integer.parseInt(tokens[0]);
+		int bRotations = Integer.parseInt(line2);
+		
+//		int actualRotations = tokens.length-bRotations;
+//		if(bRotations > arrayLength) {
+		int actualRotations = (bRotations%arrayLength);
+//		}
+		
+		for (int i = arrayLength - actualRotations+1 ; i < tokens.length; i++) {
+			System.out.print(tokens[i] + " ");
+		}
+		for (int i = 1; i < arrayLength - actualRotations+1 ; i++) {
+			System.out.print(tokens[i]+" " );
+		}
+
+		scanner.close();
+	}
+}
