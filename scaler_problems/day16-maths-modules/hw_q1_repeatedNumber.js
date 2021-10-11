34828149f5b8869abb22da940f5805904ca29cc5
Sanket <sanketwakhare@gmail.com>
2021-10-11 16:47:38 +0530

    using BigInt

--- scaler_problems/day16-maths-modules/hw_q1_repeatedNumber.js
+++ scaler_problems/day16-maths-modules/hw_q1_repeatedNumber.js
@@ -1,35 +1,36 @@
-const repeatedNumber = function(A){
-        
+const repeatedNumber = function (A) {
+
     const N = A.length;
-    
-    let sum = 0;
-    let squareSum = 0;
-    let arrSum = 0;
-    let arrSquareSum = 0;
-    
+
+    let sum = BigInt(0);
+    let squareSum = BigInt(0);
+    let arrSum = BigInt(0);
+    let arrSquareSum = BigInt(0);
+
     // for(let i=1; i<=N; i++) {
     //     sum = sum + i;
     //     squareSum = squareSum + i*i;
     // }
-    
-    for(let i=1; i<=N; i++) {
-        
-        sum = sum + i;
-        squareSum = squareSum + i*i;
-        
-        const currentElement = A[i-1];
+
+    for (let i = 1; i <= N; i++) {
+
+        sum = sum + BigInt(i);
+        squareSum = squareSum + BigInt(i * i);
+
+        const currentElement = BigInt(A[i - 1]);
         arrSum = arrSum + currentElement;
-        arrSquareSum = arrSquareSum + (currentElement*currentElement);
+        arrSquareSum = arrSquareSum + BigInt(currentElement * currentElement);
     }
-    
+
     let duplicate;
     let missing;
-    
-    duplicate = Math.floor(( Math.floor((squareSum - arrSquareSum) / (sum - arrSum)) - sum + arrSum) / 2);
-    missing = (sum - arrSum) + duplicate;
-    
+
+    duplicate = ((((BigInt(squareSum - arrSquareSum) / BigInt(sum - arrSum)) - sum + arrSum) / BigInt(2)));
+    missing = BigInt(sum - arrSum) + BigInt(duplicate);
+
     return [duplicate, missing];
-    
+
 }
 
-repeatedNumber([3,1,2,5,3]);
+repeatedNumber([3, 1, 2, 5, 3]);
+repeatedNumber([2, 5, 4, 5, 3]);
