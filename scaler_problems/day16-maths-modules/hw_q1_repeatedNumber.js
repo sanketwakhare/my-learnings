4889d262fef8c575831a5f9fbaa0f27217ea8707
Sanket <sanketwakhare@gmail.com>
2021-10-12 08:45:28 +0530

    repeated number

--- scaler_problems/day16-maths-modules/hw_q1_repeatedNumber.js
+++ scaler_problems/day16-maths-modules/hw_q1_repeatedNumber.js
@@ -1,3 +1,23 @@
+/***
+ * Repeat and Missing Number Array
+You are given a read only array of n integers from 1 to n.
+Each integer appears exactly once except A which appears twice and B which is missing.
+Return A and B.
+
+Note: Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+Note that in your output A should precede B.
+
+Example:
+Input:[3 1 2 5 3] 
+Output:[3, 4] 
+A = 3, B = 4
+ */
+
+/**
+ * TC: O(N), SC: O(1)
+ * @param {Array} A 
+ * @returns 
+ */
 const repeatedNumber = function (A) {
 
     const N = A.length;
