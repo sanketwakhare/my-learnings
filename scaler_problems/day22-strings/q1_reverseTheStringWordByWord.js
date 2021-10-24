fe6aac2541426faeba5c036813f437ceafeff2b5
Sanket <sanketwakhare@gmail.com>
2021-10-24 14:39:15 +0530

    reverse string word by word 500 pts

--- scaler_problems/day22-strings/q1_reverseTheStringWordByWord.js
+++ scaler_problems/day22-strings/q1_reverseTheStringWordByWord.js
@@ -1,3 +1,50 @@
+/****
+ * Reverse the String
+Problem Description
+
+Given a string A of size N.
+Return the string A after reversing the string word by word.
+
+NOTE:
+A sequence of non-space characters constitutes a word.
+Your reversed string should not contain leading or trailing spaces, even if it is present in the input string.
+If there are multiple spaces between words, reduce them to a single space in the reversed string.
+
+Problem Constraints
+1 <= N <= 3 * 10^5
+
+Input Format
+The only argument given is string A.
+
+Output Format
+Return the string A after reversing the string word by word.
+
+Example Input
+Input 1:
+    A = "the sky is blue"
+Input 2:
+    A = "this is ib"  
+
+Example Output
+Output 1:
+    "blue is sky the"
+Output 2:
+    "ib is this"    
+
+Example Explanation
+Explanation 1:
+    We reverse the string word by word so the string becomes "the sky is blue".
+Explanation 2:
+    We reverse the string word by word so the string becomes "this is ib".
+ */
+
+/**
+ * Util function
+ * Reverse given sub string from given start to end position
+ * @param {Array} s character array
+ * @param {*} start start index
+ * @param {*} end end index
+ */
 const reverse = (s, start, end) => {
     while (start < end) {
         let temp = s[start];
@@ -6,11 +53,12 @@ const reverse = (s, start, end) => {
         start++;
         end--;
     }
-
 }
 
-
-
+/**
+ * Reverse index string word by word
+ * @param {String} A input string
+ */
 const reverseStringWordByWord = (A) => {
     let str = A.split('');
 
@@ -43,4 +91,5 @@ const reverseStringWordByWord = (A) => {
     console.log(str.join('').trim());
 }
 
-reverseStringWordByWord(" the sky  is blue ");
+reverseStringWordByWord(" the sky is blue ");
+reverseStringWordByWord("love hate data structures");
