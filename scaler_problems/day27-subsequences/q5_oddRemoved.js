600f66fc7a7780840c8bfe803d41b2d221f54dea
Sanket <sanketwakhare@gmail.com>
2021-11-14 00:12:04 +0530

    add test cases

--- scaler_problems/day27-subsequences/q5_oddRemoved.js
+++ scaler_problems/day27-subsequences/q5_oddRemoved.js
@@ -67,6 +67,14 @@ const oddRemoved = (A) => {
         }
         result.push(innerArray);
     }
-
+    console.log(result);
     return result;
 }
+
+oddRemoved([[1, 2, 3, 4],
+[4, 5, 6, 7],
+[8, 9, 10, 11],
+[13, 15, 17, 19]]);
+
+oddRemoved([[5, 17, 1],
+[2, 2, 6]]);
