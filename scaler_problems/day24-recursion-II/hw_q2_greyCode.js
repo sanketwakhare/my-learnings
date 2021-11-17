4d31aa987016c62f92dd165a070745202641e1e6
Sanket <sanketwakhare@gmail.com>
2021-11-17 23:05:47 +0530

    grey code solution approach

--- scaler_problems/day24-recursion-II/hw_q2_greyCode.js
+++ scaler_problems/day24-recursion-II/hw_q2_greyCode.js
@@ -80,3 +80,45 @@ console.log(grayCode(5));
 console.log(grayCode(2));
 console.log(grayCode(10));
 console.log(grayCode(16));
+
+/**
+ * Solution Approach:
+ *
+ * We can exploits one of the Grey code property.
+
+Initially,  lets say the first grey code is 0 and output array is [0]
+
+Step 1:
+Try adding 2^0 to elements in array from reverse direction
+
+current output array = [0] and 2^0 = 1
+0+ 2^0 = 0+1 = 1
+The output array after step 1 becomes [0,1]
+
+Step 2:
+Repeat same thing.
+Try adding 2^1 to elements in array [0,1] from reverse direction
+
+current output array = [0,1] and 2^1 = 2
+
+1+2 = 3
+0+2 = 2
+So next two elements in output array should be 3 and 2
+The output array after step 2 is: [0,1,3,2]
+
+Step 3:
+Repeat same thing.
+Try adding 2^2 to elements in array [0,1,3,2] from reverse direction
+
+current output array = [0,1,3,2] and 2^2 = 4
+
+2+4 = 6
+3+4 = 7
+1+4 = 5
+0+4 = 4
+So next set of grey codes in output array should be [6,7,5,4]
+output array after step 3 becomes : [0,1,3,2,6,7,5,4]
+
+This way you can generate grey code.
+I hope this helps.
+ */
