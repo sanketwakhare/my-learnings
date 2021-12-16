5d916ea2ea6953a468742ef888007a1d054f9683
Sanket <sanketwakhare@gmail.com>
2021-12-16 11:54:13 +0530

    Recursive Relation TC

--- my_problems/day41-dsa-recursion-1/q1_powerModRecursive.js
+++ my_problems/day41-dsa-recursion-1/q1_powerModRecursive.js
@@ -1,3 +1,28 @@
+/*****
+ * Implement Power Function
+ * 
+Problem Description
+
+Implement pow(x, n) % d.
+In other words, given x, n and d,
+
+find (xn % d)
+
+Note that remainders on division cannot be negative. In other words, make sure the answer you return is non negative.
+
+Input : x = 2, n = 3, d = 3
+Output : 2
+
+2^3 % 3 = 8 % 3 = 2.
+ */
+
+/**
+ * Power Mod recursive function
+ * @param {Number} A base
+ * @param {Number} N power
+ * @param {Number} D mod value
+ * @returns 
+ */
 const powerRec = (A, N, D) => {
 
     // base conditions
@@ -24,6 +49,36 @@ const powerRec = (A, N, D) => {
     return BigInt(ans % D);
 };
 
+/****
+ * Recursive Relation Proof:
+ * TC: O(logN)
+ * Proof: 
+ * Assumption: T(0) = 1 and T(1) = 1 for base conditions
+ * 
+ * T(N) = T(N/2) + 1
+ *  Use substitution method
+ *  T(N/2) = T(N/4) + 1
+ * 
+ * T(N) = T(N/4) + 2
+ * 
+ *  T(N/4) = T(N/16) + 1
+ * 
+ * T(N) = T(N/16) + 3
+ * 
+ * T(N) = T(N/2^k) + k-1
+ * 
+ * 
+ * N/2^k = 1 => N = 2^k => Take log o both side
+ * 
+ * logN base 2 = log 2^k base 2 = k
+ * 
+ * logN = K
+ * 
+ * T(N) = T(1) + logN = 1+ logN = logN
+ * 
+ * TC: O(logN)
+ * 
+ */
 //param A : integer
 //param B : integer
 //param C : integer
