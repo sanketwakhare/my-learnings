32ecd50911a0dabaea0d069bb99121c4ab9d9f81
Sanket <sanketwakhare@gmail.com>
2021-12-23 22:57:08 +0530

    bubble sort: bring smallest ele to first

---
+++ my_problems/day43-dsa-sorting-1/class_bubble_sort.js
@@ -0,0 +1,28 @@
+/*****
+ * Bubble Sort
+ * 
+ * Swap adjacent elements if A[i] > A[i+1]
+ * Idea is to push smallest element to the start
+ */
+
+/**
+ * TC: O(N^2)
+ * SC: O(1)
+ * @param {Array} A array of integers
+ */
+const bubbleSort = (A) => {
+
+    for (let i = 0; i < A.length; i++) {
+        for (let j = i + 1; j < A.length; j++) {
+            if (A[i] > A[j]) {
+                // swap A[i] and A[j]
+                A[i] = A[i] ^ A[j];
+                A[j] = A[i] ^ A[j];
+                A[i] = A[i] ^ A[j];
+            }
+        }
+    }
+    console.log(A);
+}
+
+bubbleSort([2, 2, 6, 3, 1, 8, 3, 5, 4, 6]);
