d801c53792563e7d205dd87be35e4b5afceea0ab
Sanket <sanketwakhare@gmail.com>
2021-12-23 22:46:26 +0530

    selection sort impl: pick minimum and swap

---
+++ my_problems/day43-dsa-sorting-1/class_selection_sort.js
@@ -0,0 +1,30 @@
+/**
+ * Selection Sort:
+ * Pick minimum and sort
+ * 
+ */
+
+const selectionSort = (A) => {
+
+    for (let i = 0; i < A.length; i++) {
+        // pick minimum value
+        let min = Number.MAX_VALUE;
+        let minInd = -1;
+        for (let j = i; j < A.length; j++) {
+            if (A[j] < min) {
+                min = A[j];
+                minInd = j;
+            }
+        }
+        if (i !== minInd) {
+            // swap A[i] and A[minInd]
+            A[i] = A[i] ^ A[minInd];
+            A[minInd] = A[i] ^ A[minInd];
+            A[i] = A[i] ^ A[minInd];
+        }
+    }
+    console.log(A);
+
+}
+
+selectionSort([2, 2, 6, 3, 1, 8, 3, 5, 4, 6]);
