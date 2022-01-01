bf98c6ab5f464cc00663c9d7655e4d07887d2ae4
Sanket <sanketwakhare@gmail.com>
2022-01-02 00:13:29 +0530

    find gcd of a and b

---
+++ my_problems/day36-maths-gcd/class_gcd_approach1_brute_force.js
@@ -0,0 +1,27 @@
+/**
+ * Find gcd of a and b
+ * TC: O(min(a,b))
+ * @param {Number} a integer
+ * @param {Number} b integer 
+ * @returns 
+ */
+const gcd_approach1 = (a, b) => {
+
+    let ans = 1;
+    const min = Math.min(a, b);
+    for (let i = min; i >= 1; i--) {
+        if (a % i === 0 && b % i === 0) {
+            ans = i;
+            break;
+        }
+    }
+    console.log('gcd of ', a, b, 'is -> ', ans);
+    return ans;
+}
+
+gcd_approach1(3, 9);
+gcd_approach1(1, 10);
+gcd_approach1(10, 1);
+gcd_approach1(101, 44);
+gcd_approach1(12, 15);
+gcd_approach1(270, 120);
