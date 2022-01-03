d5fda570dafcd940729894c00d3dfe1fe1088cae
Sanket <sanketwakhare@gmail.com>
2022-01-04 00:51:14 +0530

    aptitude solution probability

---
+++ my_problems/day37-maths-prime/hw_q2_Aptitude47.js
@@ -0,0 +1,34 @@
+/**
+ * Aptitude 47
+ * 
+ * If two dice are thrown, what is the probability that the sum of the two dice will be a prime number?
+ */
+
+/* Solution:
+
+    max sum = 12
+    min sum = 2
+
+    prime numbers from 1 to 12 are [2, 3, 5, 7, 11]
+
+    all combinations => 
+    1+1
+    1+2 and 2+1
+    1+4 and 4+1
+    1+6 and 6+1
+    2+3 and 3+2
+    2+5 and 5+2
+    3+4 and 4+3
+    5+6 and 6+5
+
+    total 15 out of 26 combination = 15/36 = 5/12
+
+ */
+
+/**
+Options:->
+A) 5/18
+B) 5/36
+B) 1/2
+D) 5/12
+ */
