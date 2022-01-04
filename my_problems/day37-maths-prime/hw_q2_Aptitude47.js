5d7e69bb877f007799640ccdfed40d0e117bb89f
Sanket <sanketwakhare@gmail.com>
2022-01-05 02:37:30 +0530

    edit: updated texts

--- my_problems/day37-maths-prime/hw_q2_Aptitude47.js
+++ my_problems/day37-maths-prime/hw_q2_Aptitude47.js
@@ -6,22 +6,23 @@
 
 /* Solution:
 
-    max sum = 12
-    min sum = 2
+    max sum possible => 12 [when dice1 top is 6 and dice2 top is 6]
+    min sum possible => 2 [when dice1 top is 1 and dice2 top is 1]
 
-    prime numbers from 1 to 12 are [2, 3, 5, 7, 11]
+    prime numbers from range 2 to 12 are [2, 3, 5, 7, 11]
 
-    all combinations => 
-    1+1
+    all combinations where [dice1 top + dice2 top = prime number]
+    
+    [1+1
     1+2 and 2+1
     1+4 and 4+1
     1+6 and 6+1
     2+3 and 3+2
     2+5 and 5+2
     3+4 and 4+3
-    5+6 and 6+5
+    5+6 and 6+5]
 
-    total 15 out of 26 combination = 15/36 = 5/12
+    total 15 combinations with prime sum out of 36 combination = 15/36 = 5/12
 
  */
 
