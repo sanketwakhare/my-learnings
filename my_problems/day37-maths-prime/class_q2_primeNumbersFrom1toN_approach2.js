1edc8ecb65f5e11012c224608085a6fa5c370ebf
Sanket <sanketwakhare@gmail.com>
2022-01-04 23:55:37 +0530

    approach 2-prime numbers

--- my_problems/day37-maths-prime/class_q2_primeNumbersFrom1toN_approach2.js
+++ my_problems/day37-maths-prime/class_q2_primeNumbersFrom1toN_approach2.js
@@ -1,32 +1,4 @@
 /**
- * Iterations: sqrt(N)
- * TC: O(sqrt(N))
- * find if given number is prime or not
- * @param {Number} N integer number
- * @returns 
- */
-const isPrime = (N) => {
-
-    let isPrime = true;
-    let count = 2;
-    // iterate from 2 to sqrt(N)
-    for (let i = 2; i * i <= N; i++) {
-        if (N % i === 0) {
-            count++;
-            if (i !== Math.floor(N / i)) {
-                // if i is a factor then N/i is also factor
-                // count only when i != N/i
-                count++;
-            }
-        }
-    }
-    if (count > 2 || N === 1) {
-        isPrime = false;
-    }
-    return isPrime;
-}
-
-/**
  * Find prime numbers from 1 to N
  * Iteration: N/2+N/3... + N/2+N/3... => logN + logN = 2 logN
  * TC: O(log(N))
