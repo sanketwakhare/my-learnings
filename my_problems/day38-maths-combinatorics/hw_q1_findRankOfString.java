fb1ef5fdcb8d45df2b1b9ae510bcacf90a19da09
Sanket <sanketwakhare@gmail.com>
2022-01-10 01:01:43 +0530

    find rank using permutation

--- my_problems/day38-maths-combinatorics/hw_q1_findRankOfString.java
+++ my_problems/day38-maths-combinatorics/hw_q1_findRankOfString.java
@@ -1,3 +1,52 @@
+/***
+ * Sorted Permutation Rank
+ * 
+ * Problem Description
+ * 
+ * Given a string A. Find the rank of the string amongst its permutations sorted
+ * lexicographically.
+ * Assume that no characters are repeated.
+ * 
+ * Note: The answer might not fit in an integer, so return your answer % 1000003
+ * 
+ * Problem Constraints
+ * 1 <= |A| <= 1000
+ * 
+ * Input Format
+ * First argument is a string A.
+ * 
+ * Output Format
+ * Return an integer denoting the rank of the given string.
+ * 
+ * Example Input
+ * Input 1:
+ * A = "acb"
+ * Input 2:
+ * A = "a"
+ * 
+ * Example Output
+ * Output 1:
+ * 2
+ * Output 2:
+ * 1
+ * 
+ * Example Explanation
+ * Explanation 1:
+ * Given A = "acb".
+ * The order permutations with letters 'a', 'c', and 'b' :
+ * abc
+ * acb
+ * bac
+ * bca
+ * cab
+ * cba
+ * So, the rank of A is 2.
+ * 
+ * Explanation 2:
+ * Given A = "a".
+ * Rank is clearly 1.
+ */
+
 public class hw_q1_findRankOfString {
 
     // find count of characters less than current character to right
@@ -12,6 +61,7 @@ public class hw_q1_findRankOfString {
     }
 
     // factorial iterative function
+    // TC: O(N)
     public static int fact(int n, int m) {
         long ans = 1;
         for (long i = 1; i <= n; i++) {
@@ -23,6 +73,12 @@ public class hw_q1_findRankOfString {
         return (int) ans;
     }
 
+    /**
+     * TC: O(N)
+     * 
+     * @param A string
+     * @return Number rank
+     */
     public static int findRank(String A) {
 
         long rank = 1;
@@ -46,10 +102,9 @@ public class hw_q1_findRankOfString {
     }
 
     public static void main(String[] args) {
-        System.out.println(findRank("string"));
-        System.out.println(findRank("acb"));
-        System.out.println(findRank("a"));
-        System.out.println(findRank("baa"));
+        System.out.println(findRank("string")); // expected output 598
+        System.out.println(findRank("acb")); // expected output 2
+        System.out.println(findRank("a")); // expected output 1
+        System.out.println(findRank("baa")); // expected output 5
     }
-
 }
