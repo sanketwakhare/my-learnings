37b7eef70aa094e614a5280749bc667e6a817945
Sanket <sanketwakhare@gmail.com>
2022-01-17 01:33:57 +0530

    spell cheker

--- my_problems/day48-dsa-binarysearch-III/hw_q2_FoodPacketsDistribution.java
+++ my_problems/day48-dsa-binarysearch-III/hw_q2_FoodPacketsDistribution.java
@@ -46,79 +46,79 @@ Explanation 2:
 
 public class hw_q2_FoodPacketsDistribution {
 
-    public static int foodPacketDistribution(int[] A, int B) {
+  public static int foodPacketDistribution(int[] A, int B) {
 
-        // find min
-        int min = A[0];
-        long sum = 0;
-        for (int i = 0; i < A.length; i++) {
-            min = Math.min(min, A[i]);
-            sum += A[i];
-        }
-
-        // edge case when B is > sum
-        if (sum < B) {
-            return 0;
-        }
-
-        // define search space
-        int l = 1;
-        int r = min;
-        // initilize answer with min value of serach space
-        int answer = l;
-
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-
-            if (check(A, B, mid)) {
-                // update answer when officeCount <= required no of office to set B
-                answer = mid;
-                // go to right as we have to find maximum min-people to server
-                l = mid + 1;
-            } else {
-                r = mid - 1;
-            }
-        }
-
-        return answer;
+    // find min
+    int min = A[0];
+    long sum = 0;
+    for (int i = 0; i < A.length; i++) {
+      min = Math.min(min, A[i]);
+      sum += A[i];
     }
 
-    public static boolean check(int[] A, int B, int mid) {
-
-        int officesCount = 0;
-        for (int i = 0; i < A.length; i++) {
-            officesCount += (A[i] / mid);
-        }
-
-        if (officesCount >= B) {
-            // return false when officeCount >= required no of office to set B
-            return true;
-        }
-        // return true when officeCount < required no of office to set B
-        return false;
+    // edge case when B is > sum
+    if (sum < B) {
+      return 0;
     }
 
-    public static void main(String[] args) {
+    // define search space
+    int l = 1;
+    int r = min;
+    // initialize answer with min value of serach space
+    int answer = l;
 
-        int[] A1 = { 10, 20, 20, 10, 30 };
-        int output1 = foodPacketDistribution(A1, 5); // expected output 10
-        System.out.println("answer ->" + output1);
+    while (l <= r) {
+      int mid = l + (r - l) / 2;
 
-        int[] A2 = { 10000, 20000, 30000 };
-        int output2 = foodPacketDistribution(A2, 6); // expected output 10000
-        System.out.println("answer ->" + output2);
-
-        int[] A3 = { 1, 1, 1 };
-        int output3 = foodPacketDistribution(A3, 4); // expected output 0
-        System.out.println("answer ->" + output3);
-
-        int[] A4 = { 2, 9, 5, 4 };
-        int output4 = foodPacketDistribution(A4, 13); // expected output 1
-        System.out.println("answer ->" + output4);
-
-        int[] A5 = { 8, 7, 1, 5, 5, 10, 10, 1, 5, 3 };
-        int output5 = foodPacketDistribution(A5, 17); // expected output 1
-        System.out.println("answer ->" + output5);
+      if (check(A, B, mid)) {
+        // update answer when officeCount <= required no of office to set B
+        answer = mid;
+        // go to right as we have to find maximum min-people to server
+        l = mid + 1;
+      } else {
+        r = mid - 1;
+      }
     }
+
+    return answer;
+  }
+
+  public static boolean check(int[] A, int B, int mid) {
+
+    int officesCount = 0;
+    for (int i = 0; i < A.length; i++) {
+      officesCount += (A[i] / mid);
+    }
+
+    if (officesCount >= B) {
+      // return false when officeCount >= required no of office to set B
+      return true;
+    }
+    // return true when officeCount < required no of office to set B
+    return false;
+  }
+
+  public static void main(String[] args) {
+
+    int[] A1 = { 10, 20, 20, 10, 30 };
+    int output1 = foodPacketDistribution(A1, 5); // expected output 10
+    System.out.println("answer ->" + output1);
+
+    int[] A2 = { 10000, 20000, 30000 };
+    int output2 = foodPacketDistribution(A2, 6); // expected output 10000
+    System.out.println("answer ->" + output2);
+
+    int[] A3 = { 1, 1, 1 };
+    int output3 = foodPacketDistribution(A3, 4); // expected output 0
+    System.out.println("answer ->" + output3);
+
+    int[] A4 = { 2, 9, 5, 4 };
+    int output4 = foodPacketDistribution(A4, 13); // expected output 1
+    System.out.println("answer ->" + output4);
+
+    int[] A5 = { 8, 7, 1, 5, 5, 10, 10, 1, 5, 3 };
+    int output5 = foodPacketDistribution(A5, 17); // expected output 1
+    System.out.println("answer ->" + output5);
+  }
 
 }
