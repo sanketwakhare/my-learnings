6339b01452c30be1c645bd06e6bc76ad70f36c0f
Sanket <sanketwakhare@gmail.com>
2022-02-08 22:14:28 +0530

    first non repeating character solution

--- my_problems/day59-dsa-queues-II/q1_FirstNonRepeatingCharacter.java
+++ my_problems/day59-dsa-queues-II/q1_FirstNonRepeatingCharacter.java
@@ -1,8 +1,60 @@
+/* First non-repeating character */
+
+/** 
+ * Problem Description
+Given a string A denoting a stream of lowercase alphabets.
+You have to make new string B. B is formed such that we have to find first non-repeating character each time a character is inserted to the stream and append it at the end to B. if no non-repeating character is found then append '#' at the end of B.
+
+Problem Constraints
+1 <= |A| <= 100000
+
+Input Format
+The only argument given is string A.
+
+Output Format
+Return a string B after processing the stream of lowercase alphabets A.
+
+Example Input
+Input 1:
+ A = "abadbc"
+Input 2:
+ A = "abcabc"
+
+Example Output
+Output 1:
+"aabbdd"
+Output 2:
+"aaabc#"
+
+Example Explanation
+Explanation 1:
+"a"      -   first non repeating character 'a'
+"ab"     -   first non repeating character 'a'
+"aba"    -   first non repeating character 'b'
+"abad"   -   first non repeating character 'b'
+"abadb"  -   first non repeating character 'd'
+"abadbc" -   first non repeating character 'd'
+Explanation 2:
+"a"      -   first non repeating character 'a'
+"ab"     -   first non repeating character 'a'
+"abc"    -   first non repeating character 'a'
+"abca"   -   first non repeating character 'b'
+"abcab"  -   first non repeating character 'c'
+"abcabc" -   no non repeating character so '#'
+*/
+
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.Queue;
 
+/**
+ * Idea: Use hashmap with frequency to identify if element is repeated or not
+ * And use queue as we have to get the first non repeating character. As queue
+ * supports the pop from first/start ->dequeue operation
+ * TC: O(N)
+ * SC: O(N) -> N for each hashmap and queue
+ */
 public class q1_FirstNonRepeatingCharacter {
 
     public static String solve(String A) {
