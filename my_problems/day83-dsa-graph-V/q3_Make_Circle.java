b4be9597fe9c0ee012a56c5b1c4140446270db34
Sanket <sanketwakhare@gmail.com>
2022-04-17 13:02:46 +0530

    save make circle solution

---
+++ my_problems/day83-dsa-graph-V/q3_Make_Circle.java
@@ -0,0 +1,164 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+// TODO: solution not working
+public class q3_Make_Circle {
+
+    public int solve(String[] A) {
+
+        // build adjacency list using HashMAp
+        Map<Character, List<Integer>> map = buildAdjList(A);
+
+        // Maintain a count variable and apply DFS considering source as any node
+        int count = 0;
+        boolean[] visited = new boolean[A.length];
+        boolean flag = false;
+
+        for (int i = 0; i < A.length; i++) {
+            int source = i;
+            flag = dfsTraversal(source, A, map, visited, count);
+            if (flag == true) {
+                break;
+            }
+        }
+
+        return flag == true ? 1 : 0;
+    }
+
+    private Map<Character, List<Integer>> buildAdjList(String[] A) {
+
+        Map<Character, List<Integer>> map = new HashMap<Character, List<Integer>>();
+
+        for (int i = 0; i < A.length; i++) {
+
+            String str1 = A[i];
+            char startChar = str1.charAt(0);
+            if (map.containsKey(startChar)) {
+                map.get(startChar).add(i);
+            } else {
+                ArrayList<Integer> list = new ArrayList<Integer>();
+                list.add(i);
+                map.put(startChar, list);
+            }
+        }
+        return map;
+    }
+
+    private boolean dfsTraversal(int source, String[] A, Map<Character, List<Integer>> map, boolean[] visited,
+            int count) {
+
+        if (count == A.length && visited[source]) {
+            return true;
+        }
+
+        if (!visited[source]) {
+            visited[source] = true;
+            count++;
+
+            String sourceStr = A[source];
+            List<Integer> neighbors = map.get(sourceStr.charAt(sourceStr.length() - 1));
+
+            if (neighbors == null) {
+                return false;
+            }
+            for (int i = 0; i < neighbors.size(); i++) {
+                int target = neighbors.get(i);
+                if (A[target] != A[source]) {
+                    if (dfsTraversal(target, A, map, visited, count)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        q3_Make_Circle t1 = new q3_Make_Circle();
+        String[] A;
+        {
+            A = new String[] { "aab", "bac", "aaa", "cda" };
+            System.out.println(t1.solve(A));
+            A = new String[] { "da", "bc", "cd", "ab" };
+            System.out.println(t1.solve(A));
+            A = new String[] { "ihgg", "gigfi", "hggii", "gh", "ihhfhfh" };
+            System.out.println(t1.solve(A));
+            A = new String[] { "g", "ggfhhgfi", "ihifhhgf" };
+            System.out.println(t1.solve(A));
+        }
+
+    }
+
+    /*
+     * Topological sorting- not working
+     * public int solve(String[] A) {
+     * 
+     * List<List<Integer>> list = new ArrayList<List<Integer>>();
+     * int[] inDegree = new int[26];
+     * for (int i = 0; i < 26; i++) {
+     * list.add(new ArrayList<Integer>());
+     * inDegree[i] = -1;
+     * }
+     * 
+     * for (int i = 0; i < A.length; i++) {
+     * String str = A[i];
+     * int u = str.charAt(0) - 'a';
+     * int v = str.charAt(str.length() - 1) - 'a';
+     * if (u != v) {
+     * list.get(u).add(v);
+     * if (inDegree[u] == -1)
+     * inDegree[u] = 0;
+     * if (inDegree[v] == -1)
+     * inDegree[v] = 0;
+     * }
+     * }
+     * for (int i = 0; i < list.size(); i++) {
+     * int u = i;
+     * int v = -1;
+     * List<Integer> neighbors = list.get(i);
+     * if (neighbors.size() > 0 && inDegree[u] == -1) {
+     * inDegree[u] = 0;
+     * }
+     * for (int j = 0; j < neighbors.size(); j++) {
+     * v = neighbors.get(j);
+     * inDegree[v]++;
+     * }
+     * }
+     * 
+     * // Apply Topological sorting
+     * int noOfNodes = 0;
+     * Queue<Integer> queue = new LinkedList<Integer>();
+     * for (int i = 0; i < inDegree.length; i++) {
+     * if (inDegree[i] == 0) {
+     * queue.add(i);
+     * }
+     * if (inDegree[i] != -1) {
+     * noOfNodes++;
+     * }
+     * }
+     * 
+     * List<Integer> path = new ArrayList<Integer>();
+     * while (!queue.isEmpty()) {
+     * int x = queue.poll();
+     * path.add(x);
+     * List<Integer> neighbors = list.get(x);
+     * for (int i = 0; i < neighbors.size(); i++) {
+     * int child = neighbors.get(i);
+     * inDegree[child]--;
+     * if (inDegree[child] == 0) {
+     * queue.add(child);
+     * }
+     * }
+     * }
+     * 
+     * if (path.size() == noOfNodes) {
+     * return 0;
+     * }
+     * 
+     * return 1;
+     * }
+     */
+}
