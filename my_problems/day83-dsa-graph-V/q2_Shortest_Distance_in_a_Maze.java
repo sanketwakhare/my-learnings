f6e0a8b5dab3907a3414d603101d768028dc2271
Sanket <sanketwakhare@gmail.com>
2022-05-07 19:14:35 +0530

    shortest distance in a maze solution

--- my_problems/day83-dsa-graph-V/q2_Shortest_Distance_in_a_Maze.java
+++ my_problems/day83-dsa-graph-V/q2_Shortest_Distance_in_a_Maze.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -67,13 +68,23 @@ Explanation 2:
  Go directly from start to destination in distance 1. */
 public class q2_Shortest_Distance_in_a_Maze {
 
-    class Pair {
-        Coordinate cell;
-        int dist;
+    // 1 - left
+    // 2 - top
+    // 3 - right
+    // 4 - down
 
-        public Pair(Coordinate cell, int dist) {
+    class QNode {
+        // current cell coordinates
+        Coordinate cell;
+        // current distance from source
+        int dist;
+        // stores the direction from which the ball has come
+        int direction;
+
+        public QNode(Coordinate cell, int dist, int direction) {
             this.cell = cell;
             this.dist = dist;
+            this.direction = direction;
         }
     }
 
@@ -95,34 +106,118 @@ public class q2_Shortest_Distance_in_a_Maze {
         int n = A.length;
         int m = A[0].length;
 
+        // initialize 4 2d arrays to store state of visited from specific direction
+        int[][] leftVisited = new int[n][m];
+        int[][] topVisited = new int[n][m];
+        int[][] rightVisited = new int[n][m];
+        int[][] bottomVisited = new int[n][m];
+
         // single source
-        Queue<Pair> queue = new LinkedList<Pair>();
-        queue.add(new Pair(new Coordinate(B[0], B[1]), 0));
-        // mark source as visited
-        A[B[0]][B[1]] = 1;
+        Queue<QNode> queue = new LinkedList<QNode>();
+        queue.add(new QNode(new Coordinate(B[0], B[1]), 0, 1));
+        queue.add(new QNode(new Coordinate(B[0], B[1]), 0, 2));
+        queue.add(new QNode(new Coordinate(B[0], B[1]), 0, 3));
+        queue.add(new QNode(new Coordinate(B[0], B[1]), 0, 4));
+        // mark all initial source(s) as visited
+        leftVisited[B[0]][B[1]] = 1;
+        topVisited[B[0]][B[1]] = 1;
+        rightVisited[B[0]][B[1]] = 1;
+        bottomVisited[B[0]][B[1]] = 1;
 
-        int[] rows = new int[] { -1, 0, 0, 1 };
-        int[] cols = new int[] { 0, -1, 1, 0 };
+        int minDistance = Integer.MAX_VALUE;
+
+        int[] rows = new int[] { 0, 0, -1, 0, 1 };
+        int[] cols = new int[] { 0, -1, 0, 1, 0 };
         while (!queue.isEmpty()) {
-            Pair pair = queue.poll();
-            Coordinate temp = pair.cell;
+            QNode currQNode = queue.poll();
+            Coordinate temp = currQNode.cell;
+            int prevDir = currQNode.direction;
 
+            int new_x = temp.x + rows[prevDir];
+            int new_y = temp.y + cols[prevDir];
             if (temp.x == C[0] && temp.y == C[1]) {
                 // reached destination
-                return pair.dist;
+                boolean isWallFound = new_x < 0 || new_y < 0 || new_x >= n || new_y >= m || A[new_x][new_y] == 1;
+                if (isWallFound) {
+                    minDistance = Math.min(minDistance, currQNode.dist);
+                }
             }
 
-            for (int i = 0; i < rows.length; i++) {
-                int new_x = temp.x + rows[i];
-                int new_y = temp.y + cols[i];
-                boolean isSafe = new_x >= 0 && new_y >= 0 && new_x < n && new_y < m && A[new_x][new_y] == 0;
+            boolean canTravelInSameDirection = new_x >= 0 && new_y >= 0 && new_x < n && new_y < m
+                    && A[new_x][new_y] == 0;
+            if (canTravelInSameDirection) {
+                // if ball can travel in same direction
+                boolean isSafe = isSafeToAddInQueue(new_x, new_y, prevDir, leftVisited, topVisited, rightVisited,
+                        bottomVisited);
                 if (isSafe) {
-                    queue.add(new Pair(new Coordinate(new_x, new_y), pair.dist + 1));
-                    A[new_x][new_y] = 1;
+                    queue.add(new QNode(new Coordinate(new_x, new_y), currQNode.dist + 1, prevDir));
+                }
+            } else {
+                // if wall is found or boundary is reached, try other possibilities except
+                // previous direction
+                for (int i = 1; i < rows.length; i++) {
+                    int newDirection = i;
+                    if (newDirection == prevDir) {
+                        continue;
+                    }
+                    new_x = temp.x + rows[i];
+                    new_y = temp.y + cols[i];
+                    boolean canTravelInNewDirection = new_x >= 0 && new_y >= 0 && new_x < n && new_y < m
+                            && A[new_x][new_y] == 0;
+                    if (canTravelInNewDirection) {
+                        boolean isSafe = isSafeToAddInQueue(new_x, new_y, newDirection, leftVisited, topVisited,
+                                rightVisited,
+                                bottomVisited);
+                        if (isSafe) {
+                            queue.add(new QNode(new Coordinate(new_x, new_y), currQNode.dist + 1, newDirection));
+                        }
+                    }
                 }
             }
         }
+        if (minDistance != Integer.MAX_VALUE) {
+            return minDistance;
+        }
         return -1;
+    }
+
+    private boolean isSafeToAddInQueue(int x, int y, int previousDirection, int[][] leftVisited,
+            int[][] topVisited,
+            int[][] rightVisited, int[][] bottomVisited) {
+
+        boolean isUnvisited = true;
+        // if there is empty space in same direction
+        switch (previousDirection) {
+            case 1:
+                if (leftVisited[x][y] == 1) {
+                    isUnvisited = false;
+                } else {
+                    leftVisited[x][y] = 1;
+                }
+                break;
+            case 2:
+                if (topVisited[x][y] == 1) {
+                    isUnvisited = false;
+                } else {
+                    topVisited[x][y] = 1;
+                }
+                break;
+            case 3:
+                if (rightVisited[x][y] == 1) {
+                    isUnvisited = false;
+                } else {
+                    rightVisited[x][y] = 1;
+                }
+                break;
+            case 4:
+                if (bottomVisited[x][y] == 1) {
+                    isUnvisited = false;
+                } else {
+                    bottomVisited[x][y] = 1;
+                }
+                break;
+        }
+        return isUnvisited;
     }
 
     public static void main(String[] args) {
@@ -138,7 +233,25 @@ public class q2_Shortest_Distance_in_a_Maze {
             };
             B = new int[] { 1, 1 };
             C = new int[] { 2, 1 };
-            t1.solve(A, B, C);
+            int minDistance = t1.solve(A, B, C);
+            System.out.println(minDistance);
+        }
+        {
+            A = new int[][] {
+                    { 0, 0, 1 },
+                    { 1, 0, 1 },
+                    { 0, 0, 0 },
+                    { 1, 0, 0 },
+                    { 0, 0, 1 },
+                    { 1, 1, 1 },
+                    { 0, 0, 1 },
+                    { 1, 0, 1 },
+                    { 0, 0, 0 }
+            };
+            B = new int[] { 3, 2 };
+            C = new int[] { 2, 0 };
+            int minDistance = t1.solve(A, B, C);
+            System.out.println(minDistance);
         }
     }
 }
