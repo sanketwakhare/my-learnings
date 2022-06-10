0aa7d33b6093736318f71539d49a858cde59381b
Sanket <sanketwakhare@gmail.com>
2022-06-10 17:22:14 +0530

    min stack refactor

--- my_problems/day56-dsa-stacks-I/hw_q1_minStack_1_extra_space.js
+++ my_problems/day56-dsa-stacks-I/hw_q1_minStack_1_extra_space.js
@@ -1,43 +1,62 @@
 /* Implement Min Stack using 2 stacks */
 
-function solve() {
-    // Initialize your variables here
-    this.mainStack = [];
-    this.minStack = [];
-}
+class MinStack {
+    constructor() {
+        // Initialize your variables here
+        this.mainStack = [];
+        this.minStack = [];
+    }
 
-solve.prototype.push = function (e) {
-    this.mainStack.push(e);
-    if (this.minStack.length === 0) {
-        this.minStack.push(e);
-    } else {
-        if (e < this.minStack[this.minStack.length - 1]) {
-            // update min
+
+    push = function (e) {
+        this.mainStack.push(e);
+        if (this.minStack.length === 0) {
             this.minStack.push(e);
         } else {
-            // do not update min
-            this.minStack.push(this.minStack[this.minStack.length - 1]);
+            if (e < this.minStack[this.minStack.length - 1]) {
+                // update min
+                this.minStack.push(e);
+            } else {
+                // do not update min
+                this.minStack.push(this.minStack[this.minStack.length - 1]);
+            }
         }
-    }
-};
-solve.prototype.pop = function () {
-    // pop element from both stacks and return from mainStack
-    if (this.mainStack.length > 0) {
-        this.minStack.pop();
-        return this.mainStack.pop();
-    }
-};
-solve.prototype.top = function () {
-    // return top
-    if (this.mainStack.length === 0) return -1;
-    return this.mainStack[this.mainStack.length - 1];
-};
-solve.prototype.getMin = function () {
-    // return minimum
-    if (this.minStack.length === 0) return -1;
-    return this.minStack[this.minStack.length - 1];
-};
-module.exports = {
-    solve: solve
-};
+    };
+    pop = function () {
+        // pop element from both stacks and return from mainStack
+        if (this.mainStack.length > 0) {
+            this.minStack.pop();
+            return this.mainStack.pop();
+        }
+    };
 
+    top = function () {
+        // return top
+        if (this.mainStack.length === 0) return -1;
+        return this.mainStack[this.mainStack.length - 1];
+    };
+
+    getMin = function () {
+        // return minimum
+        if (this.minStack.length === 0) return -1;
+        return this.minStack[this.minStack.length - 1];
+    };
+}
+
+let minStack = new MinStack();
+minStack.push(6);
+minStack.push(4);
+minStack.push(7);
+minStack.push(2);
+
+console.log('top', minStack.top());
+console.log('min', minStack.getMin());
+console.log('pop', minStack.pop());
+
+console.log('top', minStack.top());
+console.log('min', minStack.getMin());
+console.log('pop', minStack.pop());
+
+console.log('top', minStack.top());
+console.log('min', minStack.getMin());
+console.log('pop', minStack.pop());
--- my_problems/day56-dsa-stacks-I/hw_q1_minStack_2_without_extra_space.js
+++ my_problems/day56-dsa-stacks-I/hw_q1_minStack_2_without_extra_space.js
@@ -2,53 +2,79 @@
 
 /* Implement getMin() using single stack */
 
-function solve() {
-    // Initalize your variables here
-    this.mainStack = [];
-    this.min = -1;
-}
+class MinStack {
 
-solve.prototype.push = function (e) {
-    if (this.mainStack.length === 0) {
-        this.mainStack.push(e);
-        this.min = e;
-    } else {
-        if(e < this.min) {
-            // update min
-            let newTop = (2 * e) - this.min;
-            this.mainStack.push(newTop);
+    // Initialize your variables here
+    constructor() {
+        this.mainStack = [];
+        this.min = -1;
+    }
+
+    push = (e) => {
+        if (this.mainStack.length === 0) {
+            this.mainStack.push(e);
             this.min = e;
         } else {
-            // do not update min
-            this.mainStack.push(e);
+            if (e < this.min) {
+                // update min
+                let newTop = (2 * e) - this.min;
+                this.mainStack.push(newTop);
+                this.min = e;
+            } else {
+                // do not update min
+                this.mainStack.push(e);
+            }
         }
-    }
-};
-solve.prototype.pop = function () {
-    // pop element from both stacks and return from mainStack
-    if (this.mainStack.length > 0) {
-        const poppedEle = this.mainStack.pop();
-        if (poppedEle < this.min) {
-            // update min
-            let newMin = (this.min * 2) - poppedEle;
-            this.min = newMin;
+    };
+
+    pop = () => {
+        // pop element from both stacks and return from mainStack
+        if (this.mainStack.length > 0) {
+            const poppedEle = this.mainStack.pop();
+            if (poppedEle < this.min) {
+                // update min
+                let newMin = (this.min * 2) - poppedEle;
+                this.min = newMin;
+                return this.min;
+            }
+            return poppedEle;
         }
-        return poppedEle;
-    }
-};
-solve.prototype.top = function () {
-    // return top
-    if (this.mainStack.length === 0) return -1;
-    if (this.mainStack[this.mainStack.length - 1] < this.min) {
+    };
+
+    top = () => {
+        // return top
+        if (this.mainStack.length === 0) return -1;
+        if (this.mainStack[this.mainStack.length - 1] < this.min) {
+            return this.min;
+        }
+        return this.mainStack[this.mainStack.length - 1];
+    };
+
+    getMin = () => {
+        // return minimum
+        if (this.mainStack.length === 0) return -1;
         return this.min;
-    }
-    return this.mainStack[this.mainStack.length - 1];
-};
-solve.prototype.getMin = function () {
-    // return minimum
-    if (this.mainStack.length === 0) return -1;
-    return this.min;
-};
-module.exports = {
-    solve: solve
-};
+    };
+}
+
+let minStack = new MinStack();
+minStack.push(6);
+minStack.push(4);
+minStack.push(7);
+minStack.push(2);
+
+console.log('top', minStack.top());
+console.log('min', minStack.getMin());
+console.log('pop', minStack.pop());
+
+console.log('top', minStack.top());
+console.log('min', minStack.getMin());
+console.log('pop', minStack.pop());
+
+console.log('top', minStack.top());
+console.log('min', minStack.getMin());
+console.log('pop', minStack.pop());
+
+console.log('top', minStack.top());
+console.log('min', minStack.getMin());
+console.log('pop', minStack.pop());
