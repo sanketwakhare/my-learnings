3267b657c9fa1d4d0a3cf66c372b6a6d94f108e5
sanketwakhare <sanketwakhare@gmail.com>
2022-08-14 15:49:51 +0530

    leetcode daily problem tle

--- leetcode/WordLadder_II.java
+++ leetcode/WordLadder_II.java
@@ -10,24 +10,24 @@ public class WordLadder_II {
             List<String> wordList = List.of("hot", "dot", "dog", "lot", "log", "cog");
             List<List<String>> result = t1.findLadders(beginWord, endWord, wordList);
         }
-//        {
-//            String beginWord = "hot";
-//            String endWord = "dog";
-//            List<String> wordList = List.of("hot", "dog");
-//            List<List<String>> result = t1.findLadders(beginWord, endWord, wordList);
-//        }
-//        {
-//            String beginWord = "hit";
-//            String endWord = "cog";
-//            List<String> wordList = List.of("hot", "dot", "dog", "lot", "log");
-//            List<List<String>> result = t1.findLadders(beginWord, endWord, wordList);
-//        }
-//        {
-//            String beginWord = "qa";
-//            String endWord = "sq";
-//            List<String> wordList = List.of("si", "go", "se", "cm", "so", "ph", "mt", "db", "mb", "sb", "kr", "ln", "tm", "le", "av", "sm", "ar", "ci", "ca", "br", "ti", "ba", "to", "ra", "fa", "yo", "ow", "sn", "ya", "cr", "po", "fe", "ho", "ma", "re", "or", "rn", "au", "ur", "rh", "sr", "tc", "lt", "lo", "as", "fr", "nb", "yb", "if", "pb", "ge", "th", "pm", "rb", "sh", "co", "ga", "li", "ha", "hz", "no", "bi", "di", "hi", "qa", "pi", "os", "uh", "wm", "an", "me", "mo", "na", "la", "st", "er", "sc", "ne", "mn", "mi", "am", "ex", "pt", "io", "be", "fm", "ta", "tb", "ni", "mr", "pa", "he", "lr", "sq", "ye");
-//            List<List<String>> result = t1.findLadders(beginWord, endWord, wordList);
-//        }
+        {
+            String beginWord = "hot";
+            String endWord = "dog";
+            List<String> wordList = List.of("hot", "dog");
+            List<List<String>> result = t1.findLadders(beginWord, endWord, wordList);
+        }
+        {
+            String beginWord = "hit";
+            String endWord = "cog";
+            List<String> wordList = List.of("hot", "dot", "dog", "lot", "log");
+            List<List<String>> result = t1.findLadders(beginWord, endWord, wordList);
+        }
+        {
+            String beginWord = "qa";
+            String endWord = "sq";
+            List<String> wordList = List.of("si", "go", "se", "cm", "so", "ph", "mt", "db", "mb", "sb", "kr", "ln", "tm", "le", "av", "sm", "ar", "ci", "ca", "br", "ti", "ba", "to", "ra", "fa", "yo", "ow", "sn", "ya", "cr", "po", "fe", "ho", "ma", "re", "or", "rn", "au", "ur", "rh", "sr", "tc", "lt", "lo", "as", "fr", "nb", "yb", "if", "pb", "ge", "th", "pm", "rb", "sh", "co", "ga", "li", "ha", "hz", "no", "bi", "di", "hi", "qa", "pi", "os", "uh", "wm", "an", "me", "mo", "na", "la", "st", "er", "sc", "ne", "mn", "mi", "am", "ex", "pt", "io", "be", "fm", "ta", "tb", "ni", "mr", "pa", "he", "lr", "sq", "ye");
+            List<List<String>> result = t1.findLadders(beginWord, endWord, wordList);
+        }
         {
             String beginWord = "cet";
             String endWord = "ism";
