21b9c4af31150d305933f7a4dbbae27a98a9e620
sanketwakhare <sanketwakhare@gmail.com>
2022-09-01 16:14:42 +0530

    graph problem no of provinces

---
+++ gfg/graph/NumberOfProvinces.java
@@ -0,0 +1,77 @@
+package graph;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class NumberOfProvinces {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader read =
+                new BufferedReader(new InputStreamReader(new FileInputStream("gfg/resources/NumberOfProvinces.txt")));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int V = Integer.parseInt(read.readLine());
+
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+            for (int i = 0; i < V; i++) {
+                String[] S = read.readLine().split(" ");
+                ArrayList<Integer> temp = new ArrayList<>();
+                for (int j = 0; j < V; j++)
+                    temp.add(Integer.parseInt(S[j]));
+                adj.add(temp);
+            }
+
+            NumberOfProvinces ob = new NumberOfProvinces();
+            System.out.println(ob.numProvinces(adj, V));
+        }
+    }
+
+    public int numProvinces(ArrayList<ArrayList<Integer>> adj, int V) {
+
+        // build adjacency list from adjacency matrix
+        int n = adj.size();
+        ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adjList.add(new ArrayList<>());
+        }
+        for (int i = 0; i < n; i++) {
+            ArrayList<Integer> row = adj.get(i);
+            ArrayList<Integer> u = adjList.get(i);
+            for (int j = 0; j < n; j++) {
+                if (row.get(j) == 1 && i != j) {
+                    ArrayList<Integer> v = adjList.get(j);
+                    u.add(j);
+                    v.add(i);
+                }
+            }
+        }
+
+        // initialize visited array
+        int[] visited = new int[n];
+        Arrays.fill(visited, -1);
+        int count = 0;
+
+        // apply dfs on every node and count total connected components
+        for (int i = 0; i < n; i++) {
+            if (visited[i] == -1) {
+                dfs(i, adjList, visited);
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public void dfs(int currNode, ArrayList<ArrayList<Integer>> adjList, int[] visited) {
+        visited[currNode] = 1;
+        for (int neighbor : adjList.get(currNode)) {
+            if (visited[neighbor] == -1) {
+                dfs(neighbor, adjList, visited);
+            }
+        }
+    }
+}
