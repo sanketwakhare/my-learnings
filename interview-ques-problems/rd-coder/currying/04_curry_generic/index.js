0ed14a8313924a726a1b7b1a33ac0e5a54fb62c4
sanketwakhare <sanketwakhare@gmail.com>
2022-09-17 22:54:33 +0530

    generic curry function

--- interview-ques-problems/rd-coder/currying/04_curry_generic/index.js
+++ interview-ques-problems/rd-coder/currying/04_curry_generic/index.js
@@ -9,21 +9,6 @@ console.log(multiply(2, 3, 4));
 
 // convert f(a,b,c) to f(a)(b)(c)
 
-// valid as well
-function curry(cb) {
-    console.log(cb.length);
-    return function curriedFunc(...args) {
-        console.log(args);
-        if (args.length < cb.length) {
-            return function (...next) {
-                return curriedFunc(...args, ...next);
-            }
-        } else {
-            return cb(...args)
-        }
-    }
-}
-
 const curriedMultiply = genericCurry(multiply);
 
 console.log(curriedMultiply(2)(3)(4));
