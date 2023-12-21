ec1f6f60169f56fb9cd4b64849f076e1b8a7e5b4
sanketwakhare <sanketwakhare@gmail.com>
2023-12-22 00:29:16 +0530

    find sum of subtree of x using segment tree after flattening tree into array

--- my_problems/dsa-competitive-programming/d12-tree-flattening/q2_subtree_sum_of_x.java
+++ my_problems/dsa-competitive-programming/d12-tree-flattening/q2_subtree_sum_of_x.java
@@ -13,24 +13,24 @@ public class q2_subtree_sum_of_x {
     public static void main(String[] args) {
         q2_subtree_sum_of_x t = new q2_subtree_sum_of_x();
         {
-            int[] A = {1, 3, 4, 2, 6, 5};
+            int[] values = {1, 3, 4, 2, 6, 5};
             int[][] B = {{1, 2}, {1, 3}, {2, 4}, {2, 5}, {3, 6}};
             int[][] Q = {{2, 1}, {1, 5, 2}, {2, 2}, {2, 1}, {2, 3}};
-            System.out.println(Arrays.toString(t.solve(A, B, Q))); // [21, 7, 17, 9]
+            System.out.println(Arrays.toString(t.solve(values, B, Q))); // [21, 7, 17, 9]
         }
     }
 
     // TC: O(Q * logN) where N is length of flattened tree which is 2 times no of nodes in tree
-    public int[] solve(int[] A, int[][] B, int[][] Q) {
+    public int[] solve(int[] values, int[][] B, int[][] Q) {
         // build adjacency list
-        int n = A.length;
+        int n = values.length;
         List<List<Integer>> adjList = buildAdjList(n, B);
 
         // populate startTime, endTime and flattened arrays
         int[] startTime = new int[n + 1];
         int[] endTime = new int[n + 1];
         int[] flattened = new int[n * 2];
-        dfs(1, -1, adjList, startTime, endTime, new int[]{0}, A, flattened);
+        dfs(1, -1, adjList, startTime, endTime, new int[]{0}, values, flattened);
 
         // build segment tree on flattened array
         int length = flattened.length;
