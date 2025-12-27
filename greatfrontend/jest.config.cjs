6f6c7085f1a7b300180e2a2be53242e68ef8b0c3
sanketwakhare <sanketwakhare@gmail.com>
2025-12-27 13:59:05 +0530

    chore: update jest.config.cjs to include moduleFileExtensions and transform settings

--- greatfrontend/jest.config.cjs
+++ greatfrontend/jest.config.cjs
@@ -1,4 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
   testMatch: ['**/*.test.js', '**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: { esModuleInterop: true } }],
+    '^.+\\.jsx?$': 'babel-jest',
+  },
 };
