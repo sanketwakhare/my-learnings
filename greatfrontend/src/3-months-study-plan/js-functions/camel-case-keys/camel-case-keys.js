fb8892758d3f981aef1a4f77fd91ae1f15a25da3
sanketwakhare <sanketwakhare@gmail.com>
2025-12-28 13:33:07 +0530

    feat: implement camel case keys function

---
+++ greatfrontend/src/3-months-study-plan/js-functions/camel-case-keys/camel-case-keys.js
@@ -0,0 +1,68 @@
+/** 
+ * 
+ * Camel Case Keys
+ * 
+ * Implement a function to convert all the keys in an object to camel case
+ * 
+ * 
+ * Implement a function camelCaseKeys, that takes an object and returns a new object with all its keys converted to camel case.
+
+Camel case is a format where words are separated with a single capitalized letter and the first letter of the word will be lower case. Some examples:
+
+String	camelCase
+foo	Yes
+fooBar	Yes
+Foo_Bar	No
+foo_bar	No
+
+For simplicity, we only need to consider the 4 string formats above, there will not be keys containing spaces, hyphens, or PascalCase.
+
+camelCaseKeys({ foo_bar: true });
+// { fooBar: true }
+
+camelCaseKeys({ foo_bar: true, bar_baz: { baz_qux: '1' } });
+// { fooBar: true, barBaz: { bazQux: '1' } }
+
+camelCaseKeys([{ baz_qux: true }, { foo: true, bar: [{ foo_bar: 'hello' }] }]);
+// [{ bazQux: true }, { foo: true, bar: [{ fooBar: 'hello' }] }]
+
+*/
+
+const capitalize = (str) => {
+  const tokens = str.split("");
+  tokens[0] = tokens[0].toUpperCase();
+  return tokens.join("");
+};
+
+const derivedValue = (value) => {
+  if (typeof value === "object") {
+    return camelCaseKeys(value);
+  }
+  return value;
+};
+
+/**
+ * @param Object
+ * @return Object
+ */
+export default function camelCaseKeys(object) {
+  if (Array.isArray(object)) {
+    return object.map((x) => camelCaseKeys(x));
+  } else if (typeof object === "object") {
+    const newObj = {};
+    for (const [key, value] of Object.entries(object)) {
+      const words = key.split("_");
+      if (words.length > 1) {
+        words[0] = words[0].toLowerCase();
+        for (let i = 1; i < words.length; i++) {
+          words[i] = capitalize(words[i]);
+        }
+        const updatedKey = words.join("");
+        newObj[updatedKey] = derivedValue(value);
+      } else {
+        newObj[key] = derivedValue(value);
+      }
+    }
+    return newObj;
+  }
+}
