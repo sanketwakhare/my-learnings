44e5548e24e22ac6b1f90c98f3671a201c08c434
sanketwakhare <sanketwakhare@gmail.com>
2026-01-01 13:34:46 +0530

    feat: implement deep clone

---
+++ greatfrontend/src/3-months-study-plan/js-functions/deep-clone/deep-clone.test.js
@@ -0,0 +1,60 @@
+import deepClone from "./deep-clone";
+
+describe("deepClone", () => {
+  describe("primitive values", () => {
+    expect(deepClone("foo")).toEqual("foo");
+    expect(deepClone(123)).toEqual(123);
+    expect(deepClone(true)).toEqual(true);
+    expect(deepClone(false)).toEqual(false);
+    expect(deepClone(null)).toEqual(null);
+  });
+
+  describe("objects", () => {
+    test("no nesting", () => {
+      const obj = { role: "foo" };
+      const clonedObj = deepClone(obj);
+      clonedObj.role = "bar";
+      expect(obj).toEqual({ role: "foo" });
+    });
+
+    test("one level of nesting", () => {
+      const obj = { user: { role: "admin", id: "123" } };
+      const clonedObj = deepClone(obj);
+      clonedObj.user.role = "bar";
+      expect(obj).toEqual({ user: { role: "admin", id: "123" } });
+    });
+
+    test("two levels of nesting", () => {
+      const obj = { a: { b: { c: "d" } }, e: "f" };
+      const clonedObj = deepClone(obj);
+      clonedObj.a.b = {};
+      expect(obj).toEqual({ a: { b: { c: "d" } }, e: "f" });
+    });
+
+    test("containing arrays", () => {
+      const obj = { foo: [{ bar: "baz" }] };
+      const clonedObj = deepClone(obj);
+      clonedObj.foo[0].bar = "bax";
+
+      expect(obj).toEqual({ foo: [{ bar: "baz" }] });
+    });
+  });
+
+  describe("arrays", () => {
+    test("containing objects", () => {
+      const obj = [{ a: "foo" }, { b: "bar" }];
+      const clonedObj = deepClone(obj);
+      clonedObj[1].b = "baz";
+
+      expect(obj).toEqual([{ a: "foo" }, { b: "bar" }]);
+    });
+
+    test("containing nested objects", () => {
+      const obj = [{ a: { id: "foo" } }, { b: { id: "baz" } }];
+      const clonedObj = deepClone(obj);
+      clonedObj[1].b = { id: "bax" };
+
+      expect(obj).toEqual([{ a: { id: "foo" } }, { b: { id: "baz" } }]);
+    });
+  });
+});
