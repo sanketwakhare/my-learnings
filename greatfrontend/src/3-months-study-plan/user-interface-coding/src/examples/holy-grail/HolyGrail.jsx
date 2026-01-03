3999efa89f3cddc6479ddf8ac3d5252316021661
sanketwakhare <sanketwakhare@gmail.com>
2026-01-03 17:00:42 +0530

    holy grails

--- greatfrontend/src/3-months-study-plan/user-interface-coding/src/examples/holy-grail/HolyGrail.jsx
+++ greatfrontend/src/3-months-study-plan/user-interface-coding/src/examples/holy-grail/HolyGrail.jsx
@@ -1,5 +1,15 @@
-import './HolyGrail.module.css';
+import styles from "./HolyGrail.module.css";
 
 export default function HolyGrail() {
-    return <div className='holyGrailContainer'>HolyGrail</div>
+  return (
+    <div className={styles.holyGrailContainer}>
+      <header>Header</header>
+      <div className={styles.columns}>
+        <nav>Navigation</nav>
+        <main>Main</main>
+        <aside>Sidebar</aside>
+      </div>
+      <footer>Footer</footer>
+    </div>
+  );
 }
